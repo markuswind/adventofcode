163d15ffddfacd64430d9c8727ffed3eb2b9b00a
Markus Wind <markuswind@gmail.com>
2017-12-05 14:50:34 +0100

    added swift base file

---
+++ 2017/base.swift
@@ -0,0 +1,26 @@
+// --------------------
+// Advent Of Code 2017
+// Day ??
+// Markus Wind
+// ---------------------
+
+class Day?? {
+
+  // MARK: - Static properties
+
+  static let puzzleInput = ""
+
+  // MARK: - Properties
+
+  // MARK: - Main method
+
+  func main() {
+  }
+
+  // MARK: - Part 1 methods
+
+  // MARK: - Part 2 methods
+
+}
+
+Day??().main()
